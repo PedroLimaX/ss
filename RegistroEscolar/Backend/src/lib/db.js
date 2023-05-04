647e7b575aa097cbdd291691be9aee00a67eb7d0
Pedro Lima <pedro99slip@gmail.com>
2023-05-04 16:52:02 -0600

    Deleted Corrupted Files

--- RegistroEscolar/Backend/src/lib/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'escolar',
-  password: ''
-});
-connection.connect();
-module.exports = connection;
